6232240796d997fff59c779757d06f3d5849a1dd
Javier Samaca <samax80@hotmail..com>
2020-03-02 11:51:06 -0500

    Fix label when loading to be empty !

--- WebApplication1/About.aspx.cs
+++ WebApplication1/About.aspx.cs
@@ -11,7 +11,7 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            Label1.Text = "";
             if (IsPostBack)
             {
                 TxtBox1.Text = "Is a Postback";
