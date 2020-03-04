6342998f2596d8e0f70f2212d133c35a8c2af09f
javier samaca <samax80@hotmail.com>
2020-03-03 20:02:05 -0500

    Create the Messages Table, create connections in order to send data to the table at the Name and MEssage fields.

--- WebApplication1/About.aspx.cs
+++ WebApplication1/About.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -17,8 +19,39 @@ namespace WebApplication1
 
         protected void SendBTN_Click(object sender, EventArgs e)
         {
-
             Label1.Text = string.Format("Status :{0}", cfMessage.Message);
+
+            /*find the connection strings, we will use this connection string 
+              to open the database connection, but before we have to create the insertStatement */
+            var connectionString = ConfigurationManager.ConnectionStrings["messageDB"].ConnectionString;
+
+            /*InsertStatemetnt will be used to insert stuff into  the database
+             SO with this one ,now we have a connection string and we have a statement we can run into the database
+             */
+            var insertStatement = "INSERT into Messages(Name,Message) values (@Name,@Message)";
+
+            /*
+             *Now we need a connection, this SqlConnection use as a parameter the connection string
+             */
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                /*we nee to open the connection*/
+
+                sqlConnection.Open();
+
+                /*
+                 */
+                using (var sqlcommand = new SqlCommand(insertStatement, sqlConnection))
+                {
+                    sqlcommand.Parameters.AddWithValue("Name", cfMessage.Message);
+                    sqlcommand.Parameters.AddWithValue("Message", cfMessage.Message);
+
+                    /* because this is not a query , this is not expected to return any data*/
+                    sqlcommand.ExecuteNonQuery();
+                }
+
+            }
+
         }
     }
 }
